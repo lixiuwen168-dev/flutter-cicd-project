1f29cd58827e708b1060d3f7a431f2d283c17554
Run Phearum <lixiuwen168@gmail.com>
2026-06-29 16:22:05 +0700

    for test in jenkins

--- lib/pages/onbaording/register_page.dart
+++ lib/pages/onbaording/register_page.dart
@@ -91,7 +91,7 @@ class _RegisterPage extends State<RegisterPage> {
             SizedBox(height: 20),
             Text(
               textAlign: TextAlign.center,
-              "update project",
+              "test on jekkin and Docker",
               style: GoogleFonts.poppins(
                 color: Colors.blueGrey,
                 fontSize: 18,
